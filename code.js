fe93ebd8b4fa3549ad21b9a31bbc3ce7e29f147d
Gabriel Fidelles <gabrielfidellesc@hotmail.com>
2021-12-06 21:23:58 -0300

    First commit: inserindo dependencias do firebase.
    Co-authored-by: matheusCMendes <matheusCMendes@users.noreply.github.com>

---
+++ code.js
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAm4UFe455sJCNAiKcd8XvmcewxnrsWPbs',
+  authDomain: 'chatrealtime-e2922.firebaseapp.com',
+  projectId: 'chatrealtime-e2922',
+  storageBucket: 'chatrealtime-e2922.appspot.com',
+  messagingSenderId: '882055935458',
+  appId: '1:882055935458:web:33f70c36b9690f134935bb',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
